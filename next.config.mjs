3475a6ddcbbe2f143d52e90fdbd79524b45d6bcb
mohdfarhan7 <farhan13886@gmail.com>
2025-07-08 16:09:12 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-import nextI18NextConfig from './next-i18next.config.js'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  ...nextI18NextConfig,
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
